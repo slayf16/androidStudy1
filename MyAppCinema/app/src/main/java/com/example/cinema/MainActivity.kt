bc1b01d8e501cfb2021d39cdb69220b71b62d6d2
Alexey Kozyrev <tcsid4@gmail.com>
2022-08-26 09:30:19 +0300

    синхронизация проекта с gradle, добавление проектов

--- MyAppCinema/app/src/main/java/com/example/cinema/MainActivity.kt
+++ MyAppCinema/app/src/main/java/com/example/cinema/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.cinema
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cinema.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
