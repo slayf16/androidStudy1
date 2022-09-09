a84d9b4c0408b5c4c16d74b6082e877672b7c718
Alexey Kozyrev <tcsid4@gmail.com>
2022-09-09 09:17:24 +0300

    разобрался с подтягиванием текста из ресурсов. Придумать что то с дизайном. Придумать что то со вторым активити, иначе кнопки в BottomNavigation бесполезны.

--- MyAppCinema/app/src/main/java/com/example/cinema/contentActivity.kt
+++ MyAppCinema/app/src/main/java/com/example/cinema/contentActivity.kt
@@ -29,51 +29,51 @@ class contentActivity : AppCompatActivity(), View.OnClickListener {
         when (stringId) {
             0 -> {
                 binding?.imageContent?.setImageResource(R.drawable.avengers)
-                binding?.longMovie?.text = R.string.longMovies.toString()
-                binding?.nameMovie?.text = R.string.Avengers.toString()
-                binding?.producerMovie?.text = R.string.producerAvengers.toString()
+                binding?.longMovie?.text = this.getText(R.string.longMovies)
+                binding?.nameMovie?.text = this.getText(R.string.Avengers)
+                binding?.producerMovie?.text = this.getText(R.string.producerAvengers)
             }
             1 -> {
                 binding?.imageContent?.setImageResource(R.drawable.millers)
-                binding?.longMovie?.text = R.string.longMovies.toString()
-                binding?.nameMovie?.text = R.string.Avengers.toString()
-                binding?.producerMovie?.text = R.string.producerAvengers.toString()
+                binding?.longMovie?.text = this.getText(R.string.longMovies)
+                binding?.nameMovie?.text = this.getText(R.string.Millers)
+                binding?.producerMovie?.text = this.getText(R.string.producerMillers)
             }
             2 -> {
                 binding?.imageContent?.setImageResource(R.drawable.star_wars)
-                binding?.longMovie?.text = R.string.longMovies.toString()
-                binding?.nameMovie?.text = R.string.Avengers.toString()
-                binding?.producerMovie?.text = R.string.producerAvengers.toString()
+                binding?.longMovie?.text = this.getText(R.string.longMovies)
+                binding?.nameMovie?.text = this.getText(R.string.StarWars)
+                binding?.producerMovie?.text = this.getText(R.string.producerStarWars)
             }
             3 -> {
                 binding?.imageContent?.setImageResource(R.drawable.ted)
-                binding?.longMovie?.text = R.string.longMovies.toString()
-                binding?.nameMovie?.text = R.string.Avengers.toString()
-                binding?.producerMovie?.text = R.string.producerAvengers.toString()
+                binding?.longMovie?.text = this.getText(R.string.longMovies)
+                binding?.nameMovie?.text = this.getText(R.string.Ted)
+                binding?.producerMovie?.text = this.getText(R.string.producerTed)
             }
             4 -> {
                 binding?.imageContent?.setImageResource(R.drawable.supernatural)
-                binding?.longMovie?.text = R.string.longMovies.toString()
-                binding?.nameMovie?.text = R.string.Avengers.toString()
-                binding?.producerMovie?.text = R.string.producerAvengers.toString()
+                binding?.longMovie?.text = this.getText(R.string.longMovies)
+                binding?.nameMovie?.text = this.getText(R.string.Supernatural)
+                binding?.producerMovie?.text = this.getText(R.string.producerSupernatural)
             }
             5 -> {
                 binding?.imageContent?.setImageResource(R.drawable.white_collars)
-                binding?.longMovie?.text = R.string.longMovies.toString()
-                binding?.nameMovie?.text = R.string.Avengers.toString()
-                binding?.producerMovie?.text = R.string.producerAvengers.toString()
+                binding?.longMovie?.text = this.getText(R.string.longMovies)
+                binding?.nameMovie?.text = this.getText(R.string.WhiteCollars)
+                binding?.producerMovie?.text = this.getText(R.string.producerWhiteCollars)
             }
             6 -> {
                 binding?.imageContent?.setImageResource(R.drawable.game_of_throne)
-                binding?.longMovie?.text = R.string.longMovies.toString()
-                binding?.nameMovie?.text = R.string.Avengers.toString()
-                binding?.producerMovie?.text = R.string.producerAvengers.toString()
+                binding?.longMovie?.text = this.getText(R.string.longMovies)
+                binding?.nameMovie?.text = this.getText(R.string.GameOfThrone)
+                binding?.producerMovie?.text = this.getText(R.string.producerGameOfThrone)
             }
             7 -> {
                 binding?.imageContent?.setImageResource(R.drawable.chernobyl)
-                binding?.longMovie?.text = R.string.longMovies.toString()
-                binding?.nameMovie?.text = R.string.Avengers.toString()
-                binding?.producerMovie?.text = R.string.producerAvengers.toString()
+                binding?.longMovie?.text = this.getText(R.string.longMovies)
+                binding?.nameMovie?.text = this.getText(R.string.Chernobyl)
+                binding?.producerMovie?.text = this.getText(R.string.producerChernobyl)
             }
         }
     }
